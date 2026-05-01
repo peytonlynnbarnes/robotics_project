d92aa9281981c9ceb25c0d09a31aa02b2dc2f241
NicoleMaia <nicky1402maia@gmail.com>
2026-05-01 11:26:05 -0400

    Update voice_command.py

--- src/voice_pkg/voice_command.py
+++ src/voice_pkg/voice_command.py
@@ -1,1 +1,13 @@
+import speech_recognition as sr
 
+def listen_for_command():
+  recognizer = sr.Recognizer()
+  with sr.Microphone() as source:
+    recognizer.adjust_for_ambient_noise(source, duration=1)
+    try:
+      audio = recognizer.listen(source, timeout=5)
+      command = recognizer.recognize_google(audio)
+      print(f"[USER RECEIVED]: {command}")
+      return command.lower()
+    except Exception:
+      return None
